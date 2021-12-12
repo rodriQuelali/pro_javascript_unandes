f9b348f7082ed3621ef8b08d0645e82b0091c035
rodri Q <rodriquelali@gmail.com>
2021-12-12 13:28:29 -0400

    operaciones btn block, none

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("holaaa");
+//console.log("holaaa");
+
+const btn = document.querySelector('#conver');
+const btninicio = document.querySelector('#inicio');
+btn.onclick = () =>{
+    document.getElementById('operacion').style.display = "block"
+    document.getElementById('infor').style.display = "none"
+    //console.log("kkkk");
+}
+
+btninicio.onclick = () =>{
+    document.getElementById('infor').style.display = "block";
+    document.getElementById('operacion').style.display = "none";
+}
